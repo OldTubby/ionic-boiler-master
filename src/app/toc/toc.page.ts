d40682431f243c2647d9bbbc2eaa71faf6cf0719
Chris Leensvaart <cleensvaart@gmail.com>
2022-01-15 17:23:46 -0600

    Initial commit of Ionic App book project.

---
+++ src/app/toc/toc.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc',
+  templateUrl: './toc.page.html',
+  styleUrls: ['./toc.page.scss'],
+})
+export class TocPage implements OnInit {
+  public chapters = [
+    {
+      title: "Chapter 1",
+      url: "/chapter1"
+    },
+    {
+      title: "Chapter 2",
+      url: "/chapter2"
+    }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
